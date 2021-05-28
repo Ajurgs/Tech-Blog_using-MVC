34cb0f6ebbb1ddcde73e697e08db50363cad7ab7
Ajurgs <ajjurgs5954@gmail.com>
2021-05-27 20:00:01 -0600

    added the posts model

---
+++ models/posts.js
@@ -0,0 +1,38 @@
+const { Model, DataType, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+  },
+  {
+    hooks: {},
+    sequelize,
+    freezeTableName: false,
+    timestamps: true,
+    underscored: false,
+    modelName: "posts",
+  }
+);
