f1b751b06515b3d8621a9ce91c09a1c4b39ff29a
Ajurgs <ajjurgs5954@gmail.com>
2021-05-29 17:29:58 -0600

    added login function

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,17 +2,24 @@ const signup = async (event) => {
   event.preventDefault();
   const username = document.querySelector("#username-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
-
+  console.log("SIGNING UP");
   if (username && password) {
     const response = await fetch("/api/users", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
     });
+    console.log(response);
     if (response.ok) {
       document.location.replace("/");
     } else {
-      alert("failed to sign up");
+      if (response.path === "password") {
+        alert("Password needs to be at least 8 characters in length!");
+      } else {
+        alert("failed to sign up");
+      }
     }
   }
 };
+
+document.querySelector("#signup-form").addEventListener("submit", signup);
