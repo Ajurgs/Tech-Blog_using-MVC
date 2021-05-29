6a81dccbdc524f376859451937177ca8fc32e6aa
Ajurgs <ajjurgs5954@gmail.com>
2021-05-29 13:50:14 -0600

    started work on home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,20 @@
 const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    res.render("homepage");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get("/login", (req, res) => {
+  try {
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const routes = require("./controllers");
 const PORT = process.env.PORT || 3001;
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sess = {
-  secret: "Super secret secret",
+  secret: process.env.SESSION_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
