2ac3b2fb9816f0b9725c91e4760c8944a4b89071
Ajurgs <ajjurgs5954@gmail.com>
2021-05-30 15:37:28 -0600

    added the new post route

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,39 +2,52 @@ const router = require("express").Router();
 const { User, Post, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
-router.get("/", withAuth, (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
     // get user posts
-    const userPosts = Post.findAll({
+    const userPosts = await Post.findAll({
       where: {
         user_id: req.session.userId,
       },
     });
-
+    console.log(userPosts);
     const posts = userPosts.map((post) => post.get({ plain: true }));
 
     res.render("dashboard", {
       posts,
+      loggedIn: req.session.loggedIn,
     });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
 
-router.get("/edit/:id", withAuth, (req, res) => {
+router.get("/edit/:id", withAuth, async (req, res) => {
   try {
-    const findPost = Post.findByPk(req.params.id);
+    const findPost = await Post.findByPk(req.params.id);
     if (findPost) {
       const post = findPost.get({ plain: true });
 
       res.render("editpost", {
         post,
+        loggedIn: req.session.loggedIn,
       });
     } else {
       res.status(404).end();
     }
   } catch (err) {
     res.redirect("homepage");
+  }
+});
+
+router.get("/new", withAuth, async (req, res) => {
+  try {
+    res.render("newpost", {
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.redirect("dashboard");
   }
 });
 module.exports = router;
