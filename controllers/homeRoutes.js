ceab3074f4d575f7d2e11009f8de8948fc2aac99
Ajurgs <ajjurgs5954@gmail.com>
2021-05-30 14:29:32 -0600

    added the single post route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,9 @@ const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
-    const postData = await Post.findAll();
+    const postData = await Post.findAll({
+      include: [User],
+    });
     const posts = postData.map((post) => post.get({ plain: true }));
     res.render("homepage", {
       posts,
@@ -12,6 +14,28 @@ router.get("/", async (req, res) => {
     });
   } catch (err) {
     console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+    if (postData) {
+      const post = postData.get({ plain: true });
+      res.render("post", { post });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
     res.status(500).json(err);
   }
 });
